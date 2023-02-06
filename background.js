f5b4e868366719da07f114529dc5e44d2e4f66d4
chayan93 <chayanbatabyal@gmail.com>
2023-02-07 00:15:35 +0530

    opening popup window - as normal tab won't open in incognito

--- background.js
+++ background.js
@@ -1,10 +1,17 @@
 var activeTabIndex = 0;
+var currentWindowId;
 
 // Called when the user clicks on the browser action.
 chrome.action.onClicked.addListener(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        activeTabIndex = tabs[0].index;
-        chrome.tabs.sendMessage(tabs[0].id, { "message": "clicked_browser_action" });
+    findCurrentWindow().then(windowId => {
+        currentWindowId = windowId;
+
+        chrome.tabs.query({ active: true, windowId: currentWindowId}, function (tabs) {
+            activeTabIndex = tabs[0].index;
+            chrome.tabs.sendMessage(tabs[0].id, { "message": "clicked_browser_action" });
+        });
+    }).catch(error => {
+        console.error(error);
     });
 });
 
@@ -14,8 +21,27 @@ chrome.runtime.onMessage.addListener(
         if (request.message === "displayJSON") {
             chrome.tabs.create({
                 url: chrome.runtime.getURL("./JSONViewer.html"),
-                index: activeTabIndex + 1
+                index: activeTabIndex + 1,
+                windowId: currentWindowId,
+                active: false
+            }, function(tab) {
+                // After the tab has been created, open a window to inject the tab
+                chrome.windows.create({
+                    tabId: tab.id,
+                    type: 'popup',
+                    focused: true
+                });
             });
         }
     }
 );
+
+const findCurrentWindow = async () => {
+    return new Promise((resolve, reject) =>{
+        chrome.windows.getCurrent({}, function (currentWindow) {
+            chrome.runtime.lastError
+            ? reject(Error(chrome.runtime.lastError.message))
+            : resolve(currentWindow.id);
+        });
+    });
+}
